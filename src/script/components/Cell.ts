3d524a9811bc62f4ccb50e83e73654dc0b164e30
miluu <hyxiong@qq.com>
2016-07-23 14:51:21 +0800

    Cell class

--- src/script/components/Cell.ts
+++ src/script/components/Cell.ts
@@ -1,5 +1,15 @@
 /// <reference path="../../../typings/index.d.ts" />
 
 export default class Cell extends createjs.Shape {
+  constructor(public cellWidth: number) {
+    super();
+    this.init();
+  }
 
+  init(): void {
+    const shapeWidth = this.cellWidth - 4
+    this.graphics
+      .beginFill('#000')
+      .drawRect(2, 2, shapeWidth, shapeWidth);
+  }
 }
--- src/script/index.ts
+++ src/script/index.ts
@@ -2,9 +2,29 @@
 
 import stage from './components/stage';
 import Board from './components/Board';
+import Cell from './components/Cell';
+import * as _ from 'lodash';
 
-const board = new Board();
+const gameConfig = {
+  CELL_WIDTH: 30,
+  COLS_COUNT: 10,
+  ROWS_COUNT: 20
+};
+
+const board = new Board(gameConfig.CELL_WIDTH, gameConfig.COLS_COUNT, gameConfig.ROWS_COUNT);
 board.x = board.y = 20;
 stage.addChild(board);
+
+let cells: Cell[] = [];
+_.times(gameConfig.COLS_COUNT, function(col) {
+  _.times(gameConfig.ROWS_COUNT, function(row) {
+    const cell = new Cell(gameConfig.CELL_WIDTH);
+    stage.addChild(cell);
+    cell.x = col * gameConfig.CELL_WIDTH + 20;
+    cell.y = row * gameConfig.CELL_WIDTH + 20;
+    cells.push(cell);
+  });
+});
+
 stage.update();
 
